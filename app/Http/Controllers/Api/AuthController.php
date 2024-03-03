412b35c3f1544ac462adb9543e871484bec4e84e
ramarfx <ramagamiing33@gmail.com>
2024-03-03 20:28:47 +0700

    auth api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['password'] = bcrypt($validated['password']);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'email atau password salah',
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(compact('user', 'token'));
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['password'] = bcrypt($validated['password']);
+
+        $user = User::create($validated);
+
+        Auth::login($user);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(compact('user', 'token'));
+    }
+}
